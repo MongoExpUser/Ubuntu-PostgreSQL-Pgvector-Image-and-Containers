6c03a60952df17537ba825475ccd574834f8d5f5
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-05-24 13:37:02 -0500

    Commit

--- weather-dql.sql
+++ weather-dql.sql
@@ -1,26 +1,27 @@
 -- weather-dql.sql
 
 -- 1.
-SELECT details ->> 'name' name,  
-  details ->> 'weather' weather_description, 
-  details ->> 'wind' wind, 
-  createdon, id  
-FROM  weather.info
+SELECT wi.details ->> 'name' name,  
+  wi.details ->> 'weather' weather_description, 
+  wi.details ->> 'wind' wind, 
+  wi.createdon, 
+  wi.id  
+FROM  weather.info wi
 ORDER BY name, id;
 
 -- 2.
-SELECT DISTINCT  details ->> 'name' name,  
-  details ->> 'name' name,
-  details ->> 'weather' weather_description, 
-  details -> 'main' ->> 'temp' as temp_deg_C,
-  details -> 'main' ->> 'humidity' as humd_percent,
-  details -> 'main' ->> 'pressure' as press_hPa,
-  details -> 'wind' ->> 'deg' as wind_direc_deg,
-  details -> 'wind' ->> 'gust' as wind_gust_meter_p_sec,
-  details -> 'wind' ->> 'speed' as wind_speed_meter_p_sec,
-  createdon,
-  id  
-FROM  weather.info 
+SELECT DISTINCT  wi.details ->> 'name' name,  
+  wi.details ->> 'name' name,
+  wi.details ->> 'weather' weather_description, 
+  wi.details -> 'main' ->> 'temp' as temp_deg_C,
+  wi.details -> 'main' ->> 'humidity' as humd_percent,
+  wi.details -> 'main' ->> 'pressure' as press_hPa,
+  wi.details -> 'wind' ->> 'deg' as wind_direc_deg,
+  wi.details -> 'wind' ->> 'gust' as wind_gust_meter_p_sec,
+  wi.details -> 'wind' ->> 'speed' as wind_speed_meter_p_sec,
+  wi.createdon,
+  wi.id  
+FROM  weather.info wi
 ORDER BY name, id;
 
 -- 3.
@@ -33,7 +34,7 @@ SELECT wi.details ->> 'name' name,
   wi.details -> 'wind' ->> 'gust' AS wind_gust_meter_p_sec,
   wi.details -> 'wind' ->> 'speed' AS wind_speed_meter_p_sec,
   wi.createdon,
-  id  
+  wi.id  
 FROM  weather.info wi
 WHERE details ->> 'name' IN (
   'Gaborone', 'Abuja', 'Akure', 'Cairo', 'Dubai', 

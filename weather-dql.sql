b9fa918f0bcae169634ad5dce9180e003eb39411
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-06-03 16:08:07 -0500

    Commit

--- weather-dql.sql
+++ weather-dql.sql
@@ -13,12 +13,12 @@ ORDER BY name, id;
 SELECT DISTINCT  wi.details ->> 'name' name,  
   wi.details ->> 'name' name,
   wi.details ->> 'weather' weather_description, 
-  wi.details -> 'main' ->> 'temp' as temp_deg_C,
-  wi.details -> 'main' ->> 'humidity' as humd_percent,
-  wi.details -> 'main' ->> 'pressure' as press_hPa,
-  wi.details -> 'wind' ->> 'deg' as wind_direc_deg,
-  wi.details -> 'wind' ->> 'gust' as wind_gust_meter_p_sec,
-  wi.details -> 'wind' ->> 'speed' as wind_speed_meter_p_sec,
+  wi.details -> 'main' ->> 'temp' AS temp_deg_C,
+  wi.details -> 'main' ->> 'humidity' AS humd_percent,
+  wi.details -> 'main' ->> 'pressure' AS press_hPa,
+  wi.details -> 'wind' ->> 'deg' AS wind_direc_deg,
+  wi.details -> 'wind' ->> 'gust' AS wind_gust_meter_p_sec,
+  wi.details -> 'wind' ->> 'speed' AS wind_speed_meter_p_sec,
   wi.createdon,
   wi.id  
 FROM  weather.info wi
@@ -27,7 +27,7 @@ ORDER BY name, id;
 -- 3.
 SELECT wi.details ->> 'name' name,  
   wi.details -> 'weather' ->0 ->> 'description' AS weather_description, 
-  wi.details -> 'main' ->> 'temp' temp_deg_C,
+  wi.details -> 'main' ->> 'temp' AS temp_deg_C,
   wi.details -> 'main' ->> 'humidity' AS humd_percent,
   wi.details -> 'main' ->> 'pressure' AS press_hPa,
   wi.details -> 'wind' ->> 'deg' AS wind_direc_deg,
@@ -41,10 +41,12 @@ WHERE details ->> 'name' IN (
   'Muscat', 'Lagos', 'Katy', 'Houston', 'Tokyo', 
   'Delhi', 'Austin',  'London', 'Moscow', 'Taipei', 
   'Edmonton', 'San Rafael', 'Helsinki', 'New York', 
-  'Los Angeles'                  
+  'Los Angeles', 'San Antonio', 'Sydney', 'Rijswijk',
+  'Bahir Dar', 'Casablanca', 'Toronto', 'Dhaka',
+  'Bangui', 'Cape Town', 'Beijing'
 )
 AND details -> 'main' ->> 'temp' > '10'
-ORDER BY name, id;
+ORDER BY name ASC, id;
 
 -- 4.
 SELECT COUNT(*) FROM  weather.info;
